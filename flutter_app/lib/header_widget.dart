2b9c9696ba508f7f06795c12243308fe14166a79
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-02-26 17:54:18 +0000

    header

---
+++ flutter_app/lib/header_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/result_widget.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(32.0),
+          bottomRight: Radius.circular(32.0),
+        ),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0Xff7755ff),
+            Color(0Xff2f2ce9),
+          ],
+        ),
+      ),
+      child: Column(
+        children: const [
+          Padding(
+            padding: EdgeInsets.only(top: 8.0),
+            child: Text(
+              'Your Result',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                height: 1.3,
+                color: Color(0xffcac9ff),
+              ),
+            ),
+          ),
+          Result(),
+          Text(
+            'Great',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              height: 1.35,
+              color: Colors.white,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 8.0, bottom: 40.0),
+            child: SizedBox(
+              width: 260.0,
+              child: Text(
+                'You scored higher than 65% of the people who have taken these test.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
+                  height: 1.35,
+                  color: Color(0xffcac9ff),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/result_widget.dart';
+import 'package:flutter_app/header_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,16 +24,8 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: [
-          Column(
-            children: const [
-              Text('Your Result'),
-              Result(),
-              Text('Great'),
-              Text(
-                  'You scored higher than 65% of the people who have taken these test.'),
-            ],
-          ),
+        children: const [
+          Header(),
         ],
       ),
     );
